73722061ef77d2dd8a10e920c08a15d6aa8f4bf4
petr-tichota <z003znje@md1y35fc.ad001.siemens.net>
2018-10-25 13:49:10 +0200

    asdasdsa

---
+++ src/udpClient.c
@@ -0,0 +1,44 @@
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#define SRV_IP "192.168.0.1"
+#define BUFLEN 512
+#define NPACK 10
+#define PORT 9930
+//12345326542649949165494994916152089542951544545454514
+void diep(char *s) {
+	perror(s);
+	exit(1);
+}
+
+int main(void)
+{
+	struct sockaddr_in si_other;
+	int s, i, slen=sizeof(si_other);
+	char buf[BUFLEN];
+
+	if ((s=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP))==-1) {
+		diep("socket");
+	}
+
+	si_other.sin_family = AF_INET;
+	si_other.sin_port = htons(PORT);
+	if (inet_aton(SRV_IP, &si_other.sin_addr)==0) {
+		fprintf(stderr, "inet_aton() failed\n");
+		exit(1);
+	}
+
+	for (i=0; i<NPACK; i++) {
+		printf("Sending packet %d\n", i);
+		sprintf(buf, "This is packet %d\n", i);
+		if (sendto(s, buf, BUFLEN, 0, &si_other, slen)==-1)
+		diep("sendto()");
+	}
+
+	close(s);
+	return 0;
+}
